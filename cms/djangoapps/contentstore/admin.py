736db15566e15f0816d4dc886aec5b4dac74c372
Jeff LaJoie <jlajoie@edx.org>
2017-07-19 16:52:58 -0400

    add to admin table

--- cms/djangoapps/contentstore/admin.py
+++ cms/djangoapps/contentstore/admin.py
@@ -5,7 +5,8 @@ Admin site bindings for contentstore
 from config_models.admin import ConfigurationModelAdmin
 from django.contrib import admin
 
-from contentstore.models import PushNotificationConfig, VideoUploadConfig
+from contentstore.models import PushNotificationConfig, VideoUploadConfig, MigrateVerifiedTrackCohortsSetting
 
 admin.site.register(VideoUploadConfig, ConfigurationModelAdmin)
 admin.site.register(PushNotificationConfig, ConfigurationModelAdmin)
+admin.site.register(MigrateVerifiedTrackCohortsSetting, ConfigurationModelAdmin)
